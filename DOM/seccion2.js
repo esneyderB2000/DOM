550bde19c2fa87f0331d0ffda3e922080abda98e
esneyderB2000 <89476846+esneyderB2000@users.noreply.github.com>
2024-04-10 09:17:34 -0500

    Update seccion2.js

--- DOM/seccion2.js
+++ DOM/seccion2.js
@@ -1,11 +1,11 @@
 const llenarSeccion2 = () => {
     const seccion2 = document.getElementById('seccion2');
     seccion2.innerHTML = `
-        <h2>Galería de Imágenes</h2>
+        <h2>Galería de Camisas / Camisetas</h2>
         <div class="gallery">
-            <img src="image4.jpg" alt="Imagen 1">
-            <img src="image5.jpg" alt="Imagen 2">
-            <img src="image6.jpg" alt="Imagen 3">
+            <img src="/Imagenes/Camiseta_Emblem_Blanco_DH6873_01_laydown.avif" alt="Imagen 1">
+            <img src="/Imagenes/png-transparent-t-shirt-adidas-sportswear-sleeve-adidas-t-shirt-tshirt-white-active-shirt.png" alt="Imagen 2">
+            <img src="/Imagenes/fd4c3c42fce802080e8c876f215c1517.jpg" alt="Imagen 3">
         </div>
     `;
 };
