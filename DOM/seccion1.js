0cf19f4c74fbaf986e26d5737d96fb59467bda10
esneyderB2000 <89476846+esneyderB2000@users.noreply.github.com>
2024-04-10 09:17:25 -0500

    seccion1.js

--- DOM/seccion1.js
+++ DOM/seccion1.js
@@ -1,11 +1,11 @@
 const llenarSeccion1 = () => {
     const seccion1 = document.getElementById('seccion1');
     seccion1.innerHTML = `
-        <h2>Galería de Imágenes</h2>
+        <h2>Galería de Zapatos / Tenis</h2>
         <div class="gallery">
-            <img src="image4.jpg" alt="Imagen 1">
-            <img src="image5.jpg" alt="Imagen 2">
-            <img src="image6.jpg" alt="Imagen 3">
+            <img src="/Imagenes/Tenis_adidas_Sleek_Blanco_EG7876_01_standard.avif" alt="Imagen 1">
+            <img src="/Imagenes/tenis-adidas-vl-court-3.0.avif" alt="Imagen 2">
+            <img src="/Imagenes/WhatsApp-Image-2020-10-28-at-5.47.17-PM-1.jpeg" alt="Imagen 3">
         </div>
     `;
 };
