ebc20bfbc60151ff55aeae2d312d2459dbb51f0d
esneyderB2000 <89476846+esneyderB2000@users.noreply.github.com>
2024-04-10 09:17:07 -0500

    main.js

--- DOM/main.js
+++ DOM/main.js
@@ -1,9 +1,11 @@
 // Importar funciones de llenado de secciones
+import { llenarSeccion0 } from './seccion0.js';
 import { llenarSeccion1 } from './seccion1.js';
 import { llenarSeccion2 } from './seccion2.js';
 import { llenarSeccion3 } from './seccion3.js';
 
 // Ejecutar funciones de llenado de secciones
+llenarSeccion0();
 llenarSeccion1();
 llenarSeccion2();
 llenarSeccion3();
@@ -12,34 +14,60 @@ llenarSeccion3();
 const aplicarEstilos = () => {
     const style = document.createElement('style');
     style.textContent = `
-        body {
-            font-family: Arial, sans-serif;
-        }
-        section {
-            margin-bottom: 20px;
-            padding: 10px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-        }
-        h2 {
-            color: #333;
-        }
-        p {
-            color: #666;
-        }
-
-        .gallery {
-            display: flex;
-            flex-wrap: wrap;
-            gap: 10px;
-        }
-        
-        .gallery img {
-            max-width: 100%;
-            height: auto;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-        }
+    body {
+        font-family: Arial, sans-serif;
+        background-color: #ccc;
+    }
+    
+    #seccion0{
+        text-align: center;
+        background-color: black;
+    }
+    
+    section {
+        margin-bottom: 20px;
+        padding: 10px;
+        border: 1px solid black;
+        border-radius: 5px;
+        background-color: #afafaf;
+    }
+    
+    h2 {
+        color: black;
+        text-align: center;
+    }
+    
+    section .h02{
+        color: white;
+    }
+    
+    p {
+        color: #666;
+    }
+    
+    .gallery {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 150px;
+        margin-bottom: 100px;
+        margin-top: 50px;
+    }
+    
+    .gallery img {
+        width: 300px;
+        height: 300px;
+        border: 5px solid #666;
+        border-radius: 5px;
+        cursor: grab;
+        box-shadow: 0px 0px 15px 5px white;
+    }
+    
+    .gallery img:hover{
+        width: 310px;
+        height: 310px;
+        transition: 1s;
+        box-shadow: 0px 0px 40px 5px black;
+    }
         
     `;
     document.head.appendChild(style);
